62327c68558f5234dcf34bcfc7e4dd046bbac111
isahxv <106313742+isahxv@users.noreply.github.com>
2023-10-20 11:06:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-
+function tocaSom (selectorAudio){
+    const elemento=document.querySelector (selectorAudio)
+    if (elements && elemento.localName==="Audio){
+      elemento.play ();
+}
+    else}
+    console.log (elemento não encontrado ou seletor inválido);
+  }
+}
+   const listaDeTeclas=document.querySelectorAll ('tecla')
+   for (let contador=0;contador<listaDeTeclas.lenght;contador++){
+   const tecla=listaDeTeclas [contador];
+   const instrumento=tecla.classList [1]
+   const; Áudio="#som_${instrumento}"
+   tecla.onclick=function (){
+     tocaSom (i Audio);
+}
+    tecla.onKeydown=function
